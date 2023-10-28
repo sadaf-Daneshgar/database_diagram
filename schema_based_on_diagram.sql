66bdc52f1b7cca9495b01a772b48852a195d6623
“sadaf <sadaf.batekzai54321@gmial.com>
2023-10-28 15:38:55 +0430

    create database and tables

---
+++ schema_based_on_diagram.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE hospital;
+
+CREATE TABLE patients (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    date_of_birth DATE
+);
+
+CREATE TABLE treatments (
+    id SERIAL PRIMARY KEY,
+    type VARCHAR(100),
+    name VARCHAR(100)
+);
+
+CREATE TABLE medical_histories (
+    id SERIAL PRIMARY KEY,
+    admitted_at TIMESTAMP,
+    patient_id INTEGER REFERENCES patients(id),
+    status VARCHAR(100)
+);
+
+CREATE TABLE invoice_items(
+    id SERIAL PRIMARY KEY,
+    unit_price DECIMAL(10,2),
+    quantity INTEGER,
+    total_price DECIMAL(10,2),
+    invoice_id INTEGER REFERENCES invoices(id),
+    treatment_id INTEGER REFERENCES treatments(id)
+);
+
+CREATE TABLE invoices (
+    id SERIAL PRIMARY KEY,
+    total_amount DECIMAL(10,2),
+    generated_at TIMESTAMP,
+    payed_at TIMESTAMP,
+    medical_history_id INTEGER REFERENCES medical_histories(id)
+);
